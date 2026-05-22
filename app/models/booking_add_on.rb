71182f00bb7014b2fc8c6c55d4bf8ae44e58389d
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2026-05-22 10:25:00 -0400

    lint

--- app/models/booking_add_on.rb
+++ app/models/booking_add_on.rb
@@ -1,5 +1,5 @@
 class BookingAddOn < ApplicationRecord
-  self.primary_key = [:booking_id, :add_on_id]
+  self.primary_key = [ :booking_id, :add_on_id ]
 
   belongs_to :booking
   belongs_to :add_on
--- db/migrate/20260522141511_create_booking_add_ons.rb
+++ db/migrate/20260522141511_create_booking_add_ons.rb
@@ -1,6 +1,6 @@
 class CreateBookingAddOns < ActiveRecord::Migration[8.1]
   def change
-    create_table :booking_add_ons, primary_key: [:booking_id, :add_on_id], force: :cascade do |t|
+    create_table :booking_add_ons, primary_key: [ :booking_id, :add_on_id ], force: :cascade do |t|
       t.integer :add_on_id, null: false
       t.integer :booking_id, null: false
       t.timestamps
